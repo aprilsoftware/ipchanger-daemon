9ee7504429a0861146faacdae7057014b79e3688
CedricNanni <cedric.nanni@aprilsoftware.com>
2023-07-23 19:00:56 +0200

    Build

--- build/debian/bullseye/amd64/postinst.sh
+++ build/debian/bullseye/amd64/postinst.sh
@@ -16,6 +16,10 @@ if [ ! -f /etc/ipchanger/ipchanger.json ]; then
     cp /usr/share/ipchanger/ipchanger.json /etc/ipchanger/ipchanger.json
 fi
 
+if [ ! -f /etc/ipchanger/logging.properties ]; then
+    cp /usr/share/ipchanger/logging.properties /etc/ipchanger/logging.properties
+fi
+
 if [ ! -f /etc/ipchanger/.aws/credentials ]; then
     cp /usr/share/ipchanger/credentials /etc/ipchanger/.aws/credentials
 fi
