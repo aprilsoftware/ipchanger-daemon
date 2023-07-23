55cc9b6cb5f2b3a7188bfb21fccb9765e761e762
CedricNanni <cedric.nanni@aprilsoftware.com>
2023-07-23 19:12:46 +0200

    Build

--- build/debian/bullseye/amd64/postinst.sh
+++ build/debian/bullseye/amd64/postinst.sh
@@ -1,9 +1,13 @@
 if ! id -u ipchanger > /dev/null 2>&1; then
     useradd --home-dir /etc/ipchanger --system ipchanger
+fi
 
+if [! $(stat --format '%U' /usr/local/ipchanger) == 'ipchanger' ] then
     chown -R ipchanger:ipchanger /usr/local/ipchanger
+fi
 
-    chown -R ipchanger:ipchanger /var/log/ipchanger
+if [! $(stat --format '%U' /var/log/ipchanger) == 'ipchanger' ] then
+    chown -R root:root /var/log/ipchanger
 fi
 
 mkdir -p /usr/lib/jvm/java-11-openjdk-amd64/lib/amd64
